9996e7c01736dd9964fb54563ae8b57d72d98641
Jerp010 <jeffmarquez2006@gmail.com>
2025-04-12 12:08:25 +0800

    Final

--- EmployeeOOP.py
+++ EmployeeOOP.py
@@ -28,26 +28,20 @@ class Employee(Person):
         return cls(name, int(age), department)
 
     @staticmethod
-    def company_motto():  # Static Method
-        return "Empowering employees to grow."
+    def company_greetings():  # Static Method
+        return "Welcome to our Company!"
 
-    def get_age(self):    # Instance Method
-        return self.__age
 
 
 def main():
-    emp1 = Employee("Alice", 30, "HR")                # Main constructor
-    emp2 = Employee.from_string("Bob-25-Sales")       # Alternate constructor
+    emp1 = Employee("Alice", 30, "HR") #First initialize constructor
+    emp2 = Employee.from_string("Bob-25-Sales") #Second initialize constructor
 
     emp1.show_info()  # Polymorphism
-    print("---")
+    print("")
     emp2.show_info()  # Polymorphism
-
-    print("Company Motto:", Employee.company_motto())  # Static Method
-    print("Bob's Age:", emp2.get_age())                # Instance Method
-
-    print("Is emp1 an instance of Person?", isinstance(emp1, Person))  # Inheritance check
-
+    print("")
+    print(Employee.company_greetings())  # Static Method
 
 if __name__ == "__main__":
     main()
